3077b6fa6fe185ae270f3da9bbf9678468860185
David C. Manuelda <StormByte@gmail.com>
2020-09-16 19:34:51 +0200

    Updated StormByte Functions

--- sys-libs/StormByte-functions/files/functions.sh
+++ sys-libs/StormByte-functions/files/functions.sh
@@ -22,6 +22,19 @@ function handleCommand() {
 	fi
 }
 
+function handleCommandWithOutput() {
+	# This helper will execute $1 outputting $2 as first text along with OK or ERROR if it failed
+	# Example
+	# handleCommand 'mount -t proc /proc "${tmp_folder}/proc"' "Mounting system..."
+	echo "${2} ... "
+	eval $1
+
+	if [ $? -ne 0 ]; then
+		cd "${current_folder}"
+		displayError "ERROR in command $1"
+	fi
+}
+
 function loadConfig() {
 	if [ -f "${workdir}/${self}.conf" ]; then
 		source "${workdir}/${self}.conf"
