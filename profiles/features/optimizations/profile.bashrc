ce34d0cf594d4327b7f9e5863e96104db33d08e4
David C. Manuelda <StormByte@gmail.com>
2026-06-18 01:21:46 +0200

    Improved polly usage

--- profiles/features/optimizations/profile.bashrc
+++ profiles/features/optimizations/profile.bashrc
@@ -1,36 +1,33 @@
 source /lib/StormByte/portage.sh
 
 local LTO_FORCED_PACKAGES="
-    app-office/libreoffice
-    dev-lang/erlang
-    dev-lang/nasm
-    mail-client/thunderbird
-    media-libs/x264
-    media-libs/x265
-    media-video/ffmpeg
-    llvm-core/clang
-    llvm-core/llvm
-    llvm-runtimes/libcxx
-    www-client/firefox
+	app-office/libreoffice
+	dev-lang/erlang
+	dev-lang/nasm
+	mail-client/thunderbird
+	media-libs/x264
+	media-libs/x265
+	media-video/ffmpeg
+	llvm-core/clang
+	llvm-core/llvm
+	llvm-runtimes/libcxx
+	www-client/firefox
 "
 local POLLY_FORCED_PACKAGES="
-    app-office/libreoffice
+	app-office/libreoffice
 	mail-client/thunderbird
-    media-gfx/gimp
+	media-gfx/gimp
 	media-libs/avidemux-core
-    media-libs/avidemux-plugins
-    media-video/ffmpeg
-    media-video/mpv
-    media-video/obs-studio
-    media-sound/audacity
-    media-sound/mixxx
+	media-libs/avidemux-plugins
+	media-video/ffmpeg
+	media-video/mpv
+	media-video/obs-studio
+	media-sound/audacity
+	media-sound/mixxx
 	www-client/firefox
 "
 
 if [[ ${EBUILD_PHASE} == "configure" ]]; then
 	list_contains "${LTO_FORCED_PACKAGES}" "${CATEGORY}/${PN}" && force_lto_vars
-	if list_contains "${POLLY_FORCED_PACKAGES}" "${CATEGORY}/${PN}"; then
-		CXXFLAGS="${CXXFLAGS} ${POLLY_FLAGS}"
-		LDFLAGS="${LDFLAGS} ${LINKER_POLLY}"
-	fi
+	list_contains "${POLLY_FORCED_PACKAGES}" "${CATEGORY}/${PN}" && force_polly_vars
 fi
--- sys-libs/StormByte-functions/files/portage.sh
+++ sys-libs/StormByte-functions/files/portage.sh
@@ -1,45 +1,51 @@
 #! /bin/bash
 
-# Version 4.0.2
+# Version 4.1.0
 
 function list_contains() { [[ "$1" =~ (^|[[:space:]])"$2"($|[[:space:]]) ]]; }
 
 function force_binutils_vars() {
 	local binutils_prefix="$(binutils-config -B)"
-    ADDR2LINE="${binutils_prefix}/addr2line"
-    AS="${binutils_prefix}/as"
-    AR="${binutils_prefix}/ar"
-    NM="${binutils_prefix}/nm"
-    OBJCOPY="${binutils_prefix}/objcopy"
-    OBJDUMP="${binutils_prefix}/objdump"
-    RANLIB="${binutils_prefix}/ranlib"
-    READELF="${binutils_prefix}/readelf"
-    STRINGS="${binutils_prefix}/strings"
-    STRIP="${binutils_prefix}/strip"
-    LDFLAGS="${LINKER_BASE} ${LINKER_BFD}"
+	ADDR2LINE="${binutils_prefix}/addr2line"
+	AS="${binutils_prefix}/as"
+	AR="${binutils_prefix}/ar"
+	NM="${binutils_prefix}/nm"
+	OBJCOPY="${binutils_prefix}/objcopy"
+	OBJDUMP="${binutils_prefix}/objdump"
+	RANLIB="${binutils_prefix}/ranlib"
+	READELF="${binutils_prefix}/readelf"
+	STRINGS="${binutils_prefix}/strings"
+	STRIP="${binutils_prefix}/strip"
+	LDFLAGS="${LINKER_BASE} ${LINKER_BFD}"
 }
 
 function force_gcc_vars() {
-    OCC="gcc"
-    OCXX="g++"
-    CC="gcc"
-    CXX="g++"
-    CPP="cpp"
-    CFLAGS="${FLAGS_BASE} ${FLAGS_CPU} ${FLAGS_GCC} ${FLAGS_SECURITY} ${FLAGS_GRAPHITE}"
-    CXXFLAGS="${CFLAGS}"
-    force_binutils_vars
+	OCC="gcc"
+	OCXX="g++"
+	CC="gcc"
+	CXX="g++"
+	CPP="cpp"
+	CFLAGS="${FLAGS_BASE} ${FLAGS_CPU} ${FLAGS_GCC} ${FLAGS_SECURITY} ${FLAGS_GRAPHITE}"
+	CXXFLAGS="${CFLAGS}"
+	force_binutils_vars
 }
 
 function force_pic_vars() {
-    CFLAGS="${CFLAGS} -fPIC"
-    CXXFLAGS="${CXXFLAGS} -fPIC"
+	CFLAGS="${CFLAGS} -fPIC"
+	CXXFLAGS="${CXXFLAGS} -fPIC"
 }
 
 function force_lto_vars() {
-    CFLAGS="${CFLAGS} ${FLAGS_LTO}"
-    CXXFLAGS="${CXXFLAGS} ${FLAGS_LTO}"
-    LDFLAGS="${LDFLAGS} ${FLAGS_LTO}"
+	CFLAGS="${CFLAGS} ${FLAGS_LTO}"
+	CXXFLAGS="${CXXFLAGS} ${FLAGS_LTO}"
+	LDFLAGS="${LDFLAGS} ${FLAGS_LTO}"
 	RUSTFLAGS="${RUSTFLAGS} -Cembed-bitcode=yes -Clto=thin -Zdylib-lto -Clinker-plugin-lto"
+}
+
+function force_polly_vars() {
+	CFLAGS="${CFLAGS} ${POLLY_FLAGS}"
+	CXXFLAGS="${CXXFLAGS} ${POLLY_FLAGS}"
+	LDFLAGS="${LDFLAGS} ${LINKER_POLLY}"
 }
 
 function force_ld_undefined_version {
